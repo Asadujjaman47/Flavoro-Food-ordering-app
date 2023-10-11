7c3a68fe99456a4363838c174b80237f5859573a
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-10-11 17:25:18 +0600

    Creating CategorySlice

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import FoodData from "../data/FoodData";
 
 function CategoryMenu() {
+  const [categories, setCategories] = useState([]);
+  const listUniqueCategories = () => {
+    const uniqueCategories = [
+      ...new Set(FoodData.map((food) => food.category)),
+    ];
+    setCategories(uniqueCategories);
+
+    console.log(uniqueCategories);
+  };
+
+  useEffect(() => {
+    listUniqueCategories();
+  }, []);
+
   return (
     <div className="mx-6">
       <h3 className="text-xl font-semibold">Find the best food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-hidden">
-        <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
+        {/* <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
           All
         </button>
         <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
@@ -18,9 +33,20 @@ function CategoryMenu() {
           Dinner
         </button>
         <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
-          Snack
-        </button>
-        </div>
+        Snack
+      </button> */}
+
+        {categories.map((category, index) => {
+          return (
+            <button
+              key={index}
+              className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white"
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
