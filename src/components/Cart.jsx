5d9fb54d95a558d9e5f711cfa1072385a083cdcd
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-10-11 15:14:53 +0600

    Adding functionality to toggle cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { IoMdClose } from "react-icons/io";
+import { FaShoppingCart } from "react-icons/fa";
 import ItemCard from "./ItemCard";
 
+import { useSelector } from "react-redux";
+
 function Cart() {
+  const [activeCart, setActiveCart] = useState(true);
+
+  const cartItems = useSelector((state) => state.cart.cart);
+  console.log(cartItems);
+
   return (
     <>
-      <div className="fixed right-0 top-0 w-full lg:w-[20vw] h-full p-5 bg-white mb-3">
+      <div
+        className={`fixed right-0 top-0 w-full lg:w-[20vw] h-full p-5 bg-white mb-3 ${
+          activeCart ? "translate-x-0" : "translate-x-full"
+        }
+        transition-all duration-500 z-50
+        `}
+      >
         <div className="flex justify-between items-center my-3">
           <span className="text-xl font-bold text-gray-800">My Order</span>
-          <IoMdClose className="border-2 border-gray-600 text-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-300 hover:border-red-300" />
+          <IoMdClose
+            onClick={() => setActiveCart(!activeCart)}
+            className="border-2 border-gray-600 text-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-300 hover:border-red-300"
+          />
         </div>
 
         <ItemCard />
@@ -25,6 +42,10 @@ function Cart() {
           </button>
         </div>
       </div>
+      <FaShoppingCart
+        onClick={() => setActiveCart(!activeCart)}
+        className="rounded-full bg-white shadow-md text-5xl p-3 fixed bottom-4 right-4"
+      />
     </>
   );
 }
